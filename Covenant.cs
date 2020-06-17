3b85f16a40148b90973eb80d36826df78bf47977
yasir-arafat <arafat.yasir2010@gmail.com>
2020-06-17 17:53:03 +0530

    Add files via upload

---
+++ Covenant.cs
@@ -0,0 +1,253 @@
+﻿// Author: Ryan Cobb (@cobbr_io)
+// Project: Covenant (https://github.com/cobbr/Covenant)
+// License: GNU GPLv3
+
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
+using System.Security.Authentication;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using McMaster.Extensions.CommandLineUtils;
+using NLog.Web;
+using NLog.Config;
+using NLog.Targets;
+
+using Covenant.Models;
+using Covenant.Core;
+using Covenant.Models.Covenant;
+
+namespace Covenant
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLineApplication app = new CommandLineApplication();
+            app.HelpOption("-? | -h | --help");
+            var UserNameOption = app.Option(
+                "-u | --username <USERNAME>",
+                "The UserName to login to the Covenant API.",
+                CommandOptionType.SingleValue
+            );
+            var PasswordOption = app.Option(
+                "-p | --password <PASSWORD>",
+                "The Password to login to the Covenant API.",
+                CommandOptionType.SingleValue
+            );
+            var ComputerNameOption = app.Option(
+                "-c | --computername <COMPUTERNAME>",
+                "The ComputerName (IPAddress or Hostname) to bind the Covenant API to.",
+                CommandOptionType.SingleValue
+            );
+
+            app.OnExecute(() =>
+            {
+                if (!File.Exists(Path.Combine(Common.CovenantSharpSploitDirectory, "SharpSploit.sln")) ||
+                    !File.Exists(Path.Combine(Common.CovenantRubeusDirectory, "Rubeus.sln")))
+                {
+                    Console.Error.WriteLine("Error: git submodules have not been initialized");
+                    Console.Error.WriteLine("Covenant's submodules can be cloned with: git clone --recurse-submodules https://github.com/cobbr/Covenant");
+                    Console.Error.WriteLine("Or initialized after cloning with: git submodule update --init --recursive");
+                    return -1;
+                }
+
+                string username = UserNameOption.Value();
+                string password = "";
+
+                if (UserNameOption.HasValue() && !PasswordOption.HasValue())
+                {
+                    Console.Write("Password: ");
+                    password = GetPassword();
+                    Console.WriteLine();
+                }
+                else if (PasswordOption.HasValue())
+                {
+                    password = PasswordOption.Value();
+                }
+
+                string CovenantBindUrl = ComputerNameOption.HasValue() ? ComputerNameOption.Value() : "0.0.0.0";
+                IPAddress address = null;
+                try
+                {
+                    address = IPAddress.Parse(CovenantBindUrl);
+                }
+                catch (FormatException)
+                {
+                    address = Dns.GetHostAddresses(CovenantBindUrl).FirstOrDefault();
+                }
+                IPEndPoint CovenantEndpoint = new IPEndPoint(address, Common.CovenantHTTPSPort);
+                string CovenantUri = (CovenantBindUrl == "0.0.0.0" ? "https://127.0.0.1:" + Common.CovenantHTTPSPort : "https://" + CovenantEndpoint);
+                var host = BuildWebHost(CovenantEndpoint, CovenantUri);
+                using (var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    var context = services.GetRequiredService<CovenantContext>();
+                    var userManager = services.GetRequiredService<UserManager<CovenantUser>>();
+                    var signInManager = services.GetRequiredService<SignInManager<CovenantUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    var configuration = services.GetRequiredService<IConfiguration>();
+                    var listenerTokenSources = services.GetRequiredService<ConcurrentDictionary<int, CancellationTokenSource>>();
+                    context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context, roleManager, listenerTokenSources).Wait();
+                    if (!context.Users.Any() && UserNameOption.HasValue() && !string.IsNullOrEmpty(password))
+                    {
+                        // TODO: create user
+                        CovenantUser user = new CovenantUser { UserName = UserNameOption.Value() };
+                        Task<IdentityResult> task = userManager.CreateAsync(user, password);
+                        task.Wait();
+                        IdentityResult userResult = task.Result;
+                        if (userResult.Succeeded)
+                        {
+                            Task t = userManager.AddToRoleAsync(user, "User");
+                            t.Wait();
+                            Task t2 = userManager.AddToRoleAsync(user, "Administrator");
+                            t2.Wait();
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine($"Error creating user: {user.UserName}");
+                            return -1;
+                        }
+                    }
+                }
+                
+                LoggingConfiguration loggingConfig = new LoggingConfiguration();
+                var consoleTarget = new ColoredConsoleTarget();
+                var fileTarget = new FileTarget();
+                loggingConfig.AddTarget("console", consoleTarget);
+                loggingConfig.AddTarget("file", fileTarget);
+                consoleTarget.Layout = @"${longdate}|${event-properties:item=EventId_Id}|${uppercase:${level}}|${logger}|${message} ${exception:format=tostring}";
+                fileTarget.Layout = @"${longdate}|${event-properties:item=EventId_Id}|${uppercase:${level}}|${logger}|${message} ${exception:format=tostring}";
+                fileTarget.FileName = Common.CovenantLogDirectory + "covenant.log";
+                loggingConfig.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, "console");
+                loggingConfig.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, "file");
+
+                var logger = NLogBuilder.ConfigureNLog(loggingConfig).GetCurrentClassLogger();
+                try
+                {
+                    logger.Debug("Starting Covenant API");
+                    if (!IsElevated())
+                    {
+                        Console.Error.WriteLine("WARNING: Running Covenant non-elevated. You may not have permission to start Listeners on low-numbered ports. Consider running Covenant elevated.");
+                    }
+                    host.Run();
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex, "Covenant stopped due to exception");
+                    throw;
+                }
+                finally
+                {
+                    NLog.LogManager.Shutdown();
+                }
+                return 0;
+            });
+            app.Execute(args);
+        }
+
+        public static IWebHost BuildWebHost(IPEndPoint CovenantEndpoint, string CovenantUri) =>
+            new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.Listen(CovenantEndpoint, listenOptions =>
+                    {
+                        listenOptions.UseHttps(httpsOptions =>
+                        {
+                            if (!File.Exists(Common.CovenantPrivateCertFile) || !File.Exists(Common.CovenantPublicCertFile))
+                            {
+                                Console.WriteLine("Creating cert...");
+                                X509Certificate2 certificate = Utilities.CreateSelfSignedCertificate(CovenantEndpoint.Address, "CN=Covenant");
+                                File.WriteAllBytes(Common.CovenantPrivateCertFile, certificate.Export(X509ContentType.Pfx));
+                                File.WriteAllBytes(Common.CovenantPublicCertFile, certificate.Export(X509ContentType.Cert));
+                            }
+                            try
+                            {
+                                httpsOptions.ServerCertificate = new X509Certificate2(Common.CovenantPrivateCertFile);
+                            }
+                            catch (CryptographicException)
+                            {
+                                Console.Error.WriteLine("Error importing Covenant certificate.");
+                            }
+                            httpsOptions.SslProtocols = SslProtocols.Tls12;
+                            Console.WriteLine("Using Covenant certificate with hash: " + httpsOptions.ServerCertificate.GetCertHashString());
+                        });
+                    });
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    string appsettingscontents = File.ReadAllText(Common.CovenantAppSettingsFile);
+                    if (appsettingscontents.Contains(Common.CovenantJwtKeyReplaceMessage))
+                    {
+                        Console.WriteLine("Found default JwtKey, replacing with auto-generated key...");
+                        File.WriteAllText(Common.CovenantAppSettingsFile, appsettingscontents.Replace(Common.CovenantJwtKeyReplaceMessage, Utilities.GenerateJwtKey()));
+                    }
+                    var env = hostingContext.HostingEnvironment;
+                    config.AddJsonFile(Common.CovenantAppSettingsFile, optional: false, reloadOnChange: false);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddFilter("System", LogLevel.Warning)
+                           .AddFilter("Microsoft", LogLevel.Warning);
+                })
+                .UseStartup<Startup>()
+                .UseSetting("CovenantUri", CovenantUri)
+                .Build();
+
+        private static bool IsElevated()
+        {
+
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+                var principal = new System.Security.Principal.WindowsPrincipal(identity);
+                return principal.IsInRole("Administrators");
+            }
+            return Environment.UserName.Equals("root", StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        private static string GetPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo nextKey = Console.ReadKey(true);
+            while (nextKey.Key != ConsoleKey.Enter)
+            {
+                if (nextKey.Key == ConsoleKey.Backspace)
+                {
+                    if (password.Length > 0)
+                    {
+                        password = password.Substring(0, password.Length - 1);
+                        Console.Write("\b \b");
+                    }
+                }
+                else
+                {
+                    password += nextKey.KeyChar;
+                    Console.Write("*");
+                }
+                nextKey = Console.ReadKey(true);
+            }
+            return password;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,277 @@
+﻿// Author: Ryan Cobb (@cobbr_io)
+// Project: Covenant (https://github.com/cobbr/Covenant)
+// License: GNU GPLv3
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.IdentityModel.Tokens.Jwt;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.HttpOverrides;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+using Covenant.Hubs;
+using Covenant.API;
+using Covenant.Core;
+using Covenant.Models;
+using Covenant.Models.Covenant;
+using System.Net;
+
+namespace Covenant
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CovenantContext>();
+
+            services.AddIdentity<CovenantUser, IdentityRole>()
+                .AddEntityFrameworkStores<CovenantContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 1;
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.RequireUniqueEmail = false;
+            });
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.KnownProxies.Add(IPAddress.Parse(Configuration["TrustedProxies"]));
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(120);
+
+                options.LoginPath = "/CovenantUser/Login";
+                options.LogoutPath = "/CovenantUser/Logout";
+                options.AccessDeniedPath = "/Login/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services.AddAuthentication()
+                .AddJwtBearer("JwtBearer", options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = Configuration["JwtIssuer"],
+                        ValidAudience = Configuration["JwtAudience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Common.CovenantEncoding.GetBytes(Configuration["JwtKey"])),
+                        ClockSkew = TimeSpan.Zero
+                    };
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+                            var path = context.HttpContext.Request.Path;
+                            if (!string.IsNullOrEmpty(accessToken) && (context.HttpContext.WebSockets.IsWebSocketRequest || context.Request.Headers["accept"] == "text/event-stream"))
+                            {
+                                context.Token = context.Request.Query["access_token"];
+                            }
+                            return System.Threading.Tasks.Task.CompletedTask;
+                        }
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.DefaultPolicy = new Microsoft.AspNetCore.Authorization.AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .AddAuthenticationSchemes("JwtBearer", "Identity.Application")
+                    .Build();
+                options.AddPolicy("RequireAdministratorRole", new Microsoft.AspNetCore.Authorization.AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .AddAuthenticationSchemes("JwtBearer", "Identity.Application")
+                    .RequireRole("Administrator")
+                    .Build());
+                options.AddPolicy("RequireJwtBearer", new Microsoft.AspNetCore.Authorization.AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .AddAuthenticationSchemes("JwtBearer")
+                    .Build());
+                options.AddPolicy("RequireJwtBearerRequireAdministratorRole", new Microsoft.AspNetCore.Authorization.AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .AddAuthenticationSchemes("JwtBearer")
+                    .RequireRole("Administrator")
+                    .Build());
+            });
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            }).SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
+            services.AddRouting(options => options.LowercaseUrls = true);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Covenant API", Version = "v0.1" });
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+                c.SchemaFilter<EnumSchemaFilter>();
+                c.SchemaFilter<AutoRestSchemaFilter>();
+            });
+
+            services.AddSignalR().AddJsonProtocol(options =>
+            {
+                options.PayloadSerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+
+            services.AddSingleton<ConcurrentDictionary<int, CancellationTokenSource>>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Covenant API V0.1");
+                });
+                app.Use((context, next) =>
+                {
+                    context.Response.Headers.Remove("Server");
+                    return next();
+                });
+            }
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost
+            });
+
+			app.UseAuthentication();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "defaultWithoutAction",
+                    template: "{controller=Home}/{id?}"
+                );
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<GruntHub>("/grunthub", options =>
+                {
+                    options.ApplicationMaxBufferSize = 2000 * 1024;
+                });
+                routes.MapHub<EventHub>("/eventhub");
+            });
+        }
+
+        public class AutoRestSchemaFilter : ISchemaFilter
+        {
+            public void Apply(Schema schema, SchemaFilterContext context)
+            {
+                var typeInfo = context.SystemType.GetTypeInfo();
+
+                if (typeInfo.IsEnum)
+                {
+                    schema.Extensions.Add(
+                        "x-ms-enum",
+                        new { name = typeInfo.Name, modelAsString = false }
+                    );
+                }
+            }
+        }
+
+        public class EnumSchemaFilter : ISchemaFilter
+        {
+            public void Apply(Schema model, SchemaFilterContext context)
+            {
+                if (model.Properties == null)
+                    return;
+
+                var enumProperties = model.Properties.Where(p => p.Value.Enum != null)
+                    .Union(model.Properties.Where(p => p.Value.Items?.Enum != null)).ToList();
+                var enums = context.SystemType.GetProperties()
+                    .Select(p => Nullable.GetUnderlyingType(p.PropertyType) ?? p.PropertyType.GetElementType() ??
+                                    p.PropertyType.GetGenericArguments().FirstOrDefault() ?? p.PropertyType)
+                    .Where(p => p.GetTypeInfo().IsEnum)
+                    .ToList();
+
+                foreach (var enumProperty in enumProperties)
+                {
+                    var enumPropertyValue = enumProperty.Value.Enum != null ? enumProperty.Value : enumProperty.Value.Items;
+                    enumPropertyValue.Type = "string";
+                    enumPropertyValue.Format = null;
+                    var enumValues = enumPropertyValue.Enum.Select(e => $"{e}").ToList();
+                    enumPropertyValue.Enum = new List<Object>(enumValues);
+                    var enumType = enums.SingleOrDefault(p =>
+                    {
+                        var enumNames = Enum.GetNames(p);
+                        if (enumNames.Except(enumValues, StringComparer.InvariantCultureIgnoreCase).Any())
+                            return false;
+                        if (enumValues.Except(enumNames, StringComparer.InvariantCultureIgnoreCase).Any())
+                            return false;
+                        return true;
+                    });
+
+                    if (enumType == null)
+                        throw new Exception($"Property {enumProperty} not found in {context.SystemType.Name} Type.");
+
+                    if (context.SchemaRegistry.Definitions.ContainsKey(enumType.Name) == false)
+                        context.SchemaRegistry.Definitions.Add(enumType.Name, enumPropertyValue);
+
+                    var schema = new Schema
+                    {
+                        Ref = $"#/definitions/{enumType.Name}"
+                    };
+                    if (enumProperty.Value.Enum != null)
+                    {
+                        model.Properties[enumProperty.Key] = schema;
+                    }
+                    else if (enumProperty.Value.Items?.Enum != null)
+                    {
+                        enumProperty.Value.Items = schema;
+                    }
+                }
+            }
+
+        }
+    }
+}
